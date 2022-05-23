a5b6483c7833bb8e7a70d439dc8f2d9a3a90d692
Tess Sluijter-Stek <tsluijter@Tokijin.localdomain>
2022-05-23 10:51:48 +0200

    That'll teach me not to use git mv to rename.

--- Lesson 5/005 - Solutions/02-ssh-testing.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-OUTPUT=$(./02-ssh.sh)
-
-[[ ($? -eq 0) && (! -z $(echo ${OUTPUT} | grep foobar)) ]] && echo "01 Ok." || echo "01 Fail."
-
--- Lesson 5/005 - Solutions/02-ssh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ssh tess@ububu << EOF
-touch /tmp/foobar
-ls /tmp
-EOF
-
